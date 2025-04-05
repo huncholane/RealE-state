b2dad23df2ab1fb6e72660ba978a69c2e21de072
huncholane <huncho@hygo.ai>
2025-04-04 20:36:58 -0700

    'using env'

--- Client/src/constant.js
+++ Client/src/constant.js
@@ -1,4 +1,17 @@
 export const constant = {
-  // baseUrl: process.env.REACT_APP_BASE_URL
-  baseUrl: "http://localhost:5001/", // Assuming we are using localhost. This should be in .env file but I want to show the problem
+    baseUrl: process.env.REACT_APP_BASE_URL,
+    // baseUrl: "http://localhost:5001/", // Assuming we are using localhost. This should be in .env file but I want to show the problem
 };
+console.log(constant);
+console.log(constant);
+console.log(constant);
+console.log(constant);
+console.log(constant);
+console.log(constant);
+console.log(constant);
+console.log(constant);
+console.log(constant);
+console.log(constant);
+console.log(constant);
+console.log(constant);
+console.log(constant);
