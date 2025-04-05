b17aba777e986f673f1106b20eff0ca025877ade
huncholane <huncho@hygo.ai>
2025-04-05 05:12:26 -0700

    'deployed with cloudflare'

--- Client/src/constant.js
+++ Client/src/constant.js
@@ -2,16 +2,3 @@ export const constant = {
     baseUrl: process.env.REACT_APP_BASE_URL,
     // baseUrl: "http://localhost:5001/", // Assuming we are using localhost. This should be in .env file but I want to show the problem
 };
-console.log(constant);
-console.log(constant);
-console.log(constant);
-console.log(constant);
-console.log(constant);
-console.log(constant);
-console.log(constant);
-console.log(constant);
-console.log(constant);
-console.log(constant);
-console.log(constant);
-console.log(constant);
-console.log(constant);
